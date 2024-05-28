829896a9506ca2301c075254750547b68e864778
João Luiz Azevedo Barbosa <164367848+joaoluizbarbosa@users.noreply.github.com>
2024-05-28 16:13:23 -0300

    Add files via upload

---
+++ js/cardExerciciosNew-NOVO.js
@@ -0,0 +1,203 @@
+ // Script para adicionar o card de um novo exercício, remove-lo e onde esse card será inserido no código
+ function addSection() {
+    let j = document.querySelectorAll('.exercicio-n').length + 1;
+    let mainTag = document.querySelector('main');
+    let section = document.createElement("section");
+    section.className = "exercicio-n";
+    section.id = "exercicio-" + j;
+    section.draggable = true; // Adicionando o atributo draggable
+    section.addEventListener('dragstart', function(event) {
+        drag(event);
+    });
+    section.innerHTML = `
+        <ul class="lista__nome-do-exercicio">
+            <li class="lista__nome-do-exercicio__item">
+                <p class="lista__nome-do-exercicio__numero">${j}</p>
+                
+
+                <select id="nomeDoExercicio-${j}" class="exercicio__nome">
+                    <option value="" disabled selected>Selecione um exercício</option>
+                </select>
+                
+                
+                <input type="checkbox" id="menu-${j}" class="container__botao">
+                <label for="menu-${j}" class="container__rotulo">
+                    <span class="cabecalho__menu-hamburguer container__imagem">≡</span>
+                </label>
+                <ul class="lista-menu">
+                    <li class="lista-menu__item"><button class="changeEx-button" onclick="toggleEdit('nomeDoExercicio-${j}')">˄˅ _alt exer</button></li>
+                    <li class="lista-menu__item"><button class="youtube-button" onclick="showYouTubeVideo('exercicio-${j}')">» _vídeo</button></li>
+                    <li class="lista-menu__item"><button class="clock-button" onclick="toggleTimer('exercicio-${j}')">Ø _timer</button></li>
+                    <li class="lista-menu__item"><button class="color-button" onclick="preventClose(event); changeColor('exercicio-${j}')">_cores</button></li>
+                    <li class="lista-menu__item"><button class="delete-section-button" onclick="removeSection('exercicio-${j}')">✖ _excluir</button></li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="lista-secButtons">
+            <li class="lista-secButtons__item"><button class="lista-secButtons__changeEx-button" onclick="toggleEdit('nomeDoExercicio-${j}')">˄˅</button></li>
+            <li class="lista-secButtons__item"><button class="lista-secButtons__youtube-button" onclick="showYouTubeVideo('exercicio-${j}')">»</button></li>
+            <li class="lista-secButtons__item"><button class="lista-secButtons__clock-button" onclick="toggleTimer('exercicio-${j}')">Ø</button></li>
+            <li class="lista-secButtons__item"><button class="lista-secButtons__color-button" onclick="preventClose(event); changeColor('exercicio-${j}')">_</button></li>
+        </ul>  
+        <div class="video-container" id="video-container-${j}"></div>
+        <div class="exercicioObs-div">
+            <textarea id="exercicioObs-${j}" class="exercicio__obs auto-expand" placeholder="Observações sobre o exercício" maxlength="300"  oninput="limitLines(this, 10)" readonly></textarea>
+            <button class="edit-button-1" onclick="toggleEdit('exercicioObs-${j}')">Editar</button>
+        </div>
+        <ul class="lista__primeira-linha">
+            <li></li>
+            <li>
+                <select class="select-repetitions">
+                    <option class="option" value="reps range">Faixa de Repetições</option>
+                    <option value="reps">Repetições</option>
+                    <option value="reps max">Repetições Máximas</option>
+                    <option value="amrap">AMRAP</option>
+                    <option value="time">Tempo</option>
+                    <option value="time range">Faixa de Tempo</option>
+                </select>             
+            </li>
+            <li>
+                <select class="select-kg">
+                    <option value="kg">KG</option>
+                    <option value="lb">LB</option>
+                </select>             
+            </li>
+            <li>
+                <select class="select-rpe">
+                    <option value="rpe range">Faixa de Int. (PSE)</option>
+                    <option value="rpe">Int. (PSE)</option>
+                    <option value="% of 1rm range">Faixa de % de 1RM</option>
+                    <option value="% of 1rm">% de 1RM</option>
+                </select>             
+            </li>
+        </ul>
+
+        <ul class="lista__aquecimento" id="lista__aquecimento-${j}">
+            <li class="lista__aquecimento__item">
+                <p class="lista__aquecimento__w">a</p>
+            </li>
+            <li class="lista__aquecimento__item">
+                <input class="lista__aquecimento__input input-reps1" type="number" value="">
+                <p>-</p>
+                <input class="lista__aquecimento__input input-reps2" type="number" value="">
+            </li>
+            <li class="lista__aquecimento__item">
+                <input class="lista__aquecimento__input input-kg" type="number" value="">
+            </li>
+            <li class="lista__aquecimento__item">
+            </li>
+            <li class="lista__aquecimento__item">
+                <button class="remove-list-button" onclick="removeList('lista__aquecimento-${j}')"><img class="trash-img" src="img/trash-branca.png">
+            </li>
+        </ul>
+
+
+        <ul id="myList${j}" class="lista__segunda-linha">
+            <li class="lista__segunda-linha__item">
+                <p>1</p>
+            </li>
+            <li class="lista__segunda-linha__item">
+                <input class="lista__segunda-linha__input input-reps1" type="number" value="">
+                <p>-</p>
+                <input class="lista__segunda-linha__input input-reps2" type="number" value="">
+            </li>
+            <li class="lista__segunda-linha__item">
+                <input class="lista__segunda-linha__input input-kg" type="number" value="">
+            </li>
+            <li class="lista__segunda-linha__item">
+                <input class="lista__segunda-linha__input input-rpe1" type="number" value="">
+                <p>-</p>
+                <input class="lista__segunda-linha__input input-rpe2" type="number" value="">
+            </li>
+            <li class="lista__segunda-linha__item">
+                <input class="input-checkbox" type="checkbox">
+            </li>
+        </ul>
+
+        <ul class="set-buttons">
+            <li class="set-buttons__item">
+                <button id="button${j}-add-w" class="add-w-button" onclick="myFunctionWSet('exercicio-${j}', this.id)">ADD a-Set</button>
+                <button id="button${j}-add" class="add-button" onclick="myFunction('exercicio-${j}', this.id)">ADD Set</button>
+                <button id="button${j}-undo" class="undo-button" onclick="undoFunction('exercicio-${j}', this.id)">EXC Set</button>
+            </li>
+        </ul>
+        
+    `;
+
+    // Adiciona manipuladores de eventos de arrastar e soltar à nova sessão
+    section.addEventListener('dragover', allowDrop);
+    section.addEventListener('drop', drop);
+    
+    // Encontrar a div "session-buttons"
+    let sessionButtons = document.querySelector('.session-buttons');
+    let timerOrg = document.querySelector('.timer-org');
+
+    // Inserir a nova sessão antes da div "session-buttons"
+    mainTag.insertBefore(section, sessionButtons);
+    mainTag.insertBefore(section, timerOrg);
+
+}
+
+    function removeLastSection() {
+        let main = document.querySelector('main');
+        let lastSection = main.lastElementChild;
+    
+    // Verificar se o último elemento filho é um botão ou uma seção
+    while (lastSection && !lastSection.id.startsWith('exercicio-')) {
+        lastSection = lastSection.previousElementSibling;
+    }
+
+    if (lastSection) {
+        lastSection.parentNode.removeChild(lastSection);
+    }
+}  
+
+function removeSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section) {
+        section.parentNode.removeChild(section);
+        
+        // Selecionar todas as seções restantes
+        let sections = document.querySelectorAll('.exercicio-n');
+        
+        // Atualizar IDs e números das seções subsequentes
+        sections.forEach((sec, index) => {
+            let newIndex = index + 1;
+            sec.id = `exercicio-${newIndex}`;
+            sec.querySelector('.lista__nome-do-exercicio__numero').textContent = newIndex;
+            sec.querySelector('.exercicio__nome').id = `nomeDoExercicio-${newIndex}`;
+            sec.querySelector('input[type="checkbox"]').id = `menu-${newIndex}`;
+            sec.querySelector('label').setAttribute('for', `menu-${newIndex}`);
+            
+            let buttons = sec.querySelectorAll('button');
+            buttons.forEach(button => {
+                if (button.classList.contains('changeEx-button')) {
+                    button.setAttribute('onclick', `toggleEdit('nomeDoExercicio-${newIndex}')`);
+                } else if (button.classList.contains('youtube-button')) {
+                    button.setAttribute('onclick', `showYouTubeVideo('exercicio-${newIndex}')`);
+                } else if (button.classList.contains('clock-button')) {
+                    button.setAttribute('onclick', `toggleTimer('exercicio-${newIndex}')`);
+                } else if (button.classList.contains('color-button')) {
+                    button.setAttribute('onclick', `preventClose(event); changeColor('exercicio-${newIndex}')`);
+                } else if (button.classList.contains('delete-section-button')) {
+                    button.setAttribute('onclick', `removeSection('exercicio-${newIndex}')`);
+                } else if (button.classList.contains('edit-button-1')) {
+                    button.setAttribute('onclick', `toggleEdit('exercicioObs-${newIndex}')`);
+                }
+            });
+
+            sec.querySelector('.video-container').id = `video-container-${newIndex}`;
+            sec.querySelector('.exercicioObs-div textarea').id = `exercicioObs-${newIndex}`;
+            sec.querySelector('.lista__aquecimento').id = `lista__aquecimento-${newIndex}`;
+            sec.querySelector('.lista__aquecimento .remove-list-button').setAttribute('onclick', `removeList('lista__aquecimento-${newIndex}')`);
+            sec.querySelector('.lista__segunda-linha').id = `myList${newIndex}`;
+            let setButtons = sec.querySelector('.set-buttons');
+            setButtons.querySelector('.add-w-button').id = `button${newIndex}-add-w`;
+            setButtons.querySelector('.add-button').id = `button${newIndex}-add`;
+            setButtons.querySelector('.undo-button').id = `button${newIndex}-undo`;
+            setButtons.querySelector('.add-w-button').setAttribute('onclick', `myFunctionWSet('exercicio-${newIndex}', this.id)`);
+            setButtons.querySelector('.add-button').setAttribute('onclick', `myFunction('exercicio-${newIndex}', this.id)`);
+            setButtons.querySelector('.undo-button').setAttribute('onclick', `undoFunction('exercicio-${newIndex}', this.id)`);
+        });
+    }
+}
---
+++ js/youTubeId.js
@@ -0,0 +1,120 @@
+const muscleGroups = {
+    triceps: [
+        { name: "Tríceps Pulley com barra reta", videoId: "M88Bt4MMpkI" },
+        { name: "Tríceps Testa", videoId: "40Cx-IfJhA0" },
+        { name: "Tríceps Francês", videoId: "_dtPoiFWZT4" }
+    ],
+    ombros: [
+        { name: "Elevação Lateral com Halteres", videoId: "ot9nwSC1JnA" },
+        { name: "Desenvolvimento de Ombros com Halteres", videoId: "5I7ogOjvdnc" },
+        { name: "Elevação Lateral na CPolia", videoId: "dgpts8LHOLc" }
+    ],
+    biceps: [
+        { name: "Rosca Direta", videoId: "mnop1123" },
+        { name: "Rosca Martelo", videoId: "qrst1415" },
+        { name: "Rosca Scott", videoId: "uvwx1617" }
+    ],
+    peito: [
+        { name: "Supino Reto", videoId: "yzab1819" },
+        { name: "Supino Inclinado", videoId: "cdef2021" },
+        { name: "Crucifixo", videoId: "ghij2223" }
+    ],
+    costas: [
+        { name: "Puxada Frontal", videoId: "klmn2425" },
+        { name: "Remada Curvada", videoId: "opqr2627" },
+        { name: "Levantamento Terra", videoId: "stuv2829" }
+    ],
+    coxa: [
+        { name: "Agachamento", videoId: "wxyz3031" },
+        { name: "Leg Press", videoId: "abcd3233" },
+        { name: "Extensão de Perna", videoId: "efgh3435" }
+    ],
+    panturrilha: [
+        { name: "Elevação de Panturrilha", videoId: "ijkl3637" },
+        { name: "Panturrilha Sentado", videoId: "mnop3839" },
+        { name: "Panturrilha em Pé", videoId: "qrst4041" }
+    ]
+};
+
+// Função para atualizar a lista de exercícios com base nos grupos musculares selecionados
+document.querySelectorAll('input[name="group"]').forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+        // Armazena as seleções antes de atualizar os selects
+        const selectedExercises = storeExerciseSelections();
+
+        // Atualiza os selects com base nos grupos musculares selecionados
+        const selectedGroups = Array.from(document.querySelectorAll('input[name="group"]:checked')).map(input => input.value);
+        const exerciseOptions = ['<option value="" disabled selected>Selecione um exercício</option>'];
+
+        selectedGroups.forEach(group => {
+            muscleGroups[group].forEach(exercise => {
+                exerciseOptions.push(`<option value="${exercise.name}" data-video-id="${exercise.videoId}">${exercise.name}</option>`);
+            });
+        });
+
+        document.querySelectorAll('.exercicio__nome').forEach(select => {
+            select.innerHTML = exerciseOptions.join('');
+        });
+
+        // Restaura as seleções após atualizar os selects
+        restoreExerciseSelections(selectedExercises);
+    });
+});
+
+// Função para mostrar o vídeo do YouTube com base no ID especificado
+function showYouTubeVideo(sectionId) {
+    const exerciseElement = document.getElementById(sectionId);
+    const selectElement = exerciseElement.querySelector('.exercicio__nome');
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const videoId = selectedOption ? selectedOption.getAttribute('data-video-id') : '';
+
+    if (videoId) {
+        const iframe = document.createElement('iframe');
+        iframe.setAttribute('width', '325');
+        iframe.setAttribute('height', '183');
+        iframe.setAttribute('src', 'https://www.youtube.com/embed/' + videoId);
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+        iframe.setAttribute('allowfullscreen', '');
+
+        const videoContainer = exerciseElement.querySelector('.video-container');
+        videoContainer.innerHTML = '';
+        videoContainer.appendChild(iframe);
+    } else {
+        alert('Por favor, selecione um exercício.');
+    }
+}
+
+// Função para adicionar novos exercícios aos grupos musculares
+function addNewExercise(group, exercise, videoId) {
+    if (muscleGroups[group]) {
+        muscleGroups[group].push({ name: exercise, videoId: videoId });
+    } else {
+        muscleGroups[group] = [{ name: exercise, videoId: videoId }];
+    }
+    document.querySelectorAll('input[name="group"]').forEach(checkbox => {
+        if (checkbox.checked) {
+            checkbox.dispatchEvent(new Event('change'));
+        }
+    });
+}
+
+// Função para armazenar as seleções de exercícios
+function storeExerciseSelections() {
+    const selectedExercises = {};
+    document.querySelectorAll('.exercicio__nome').forEach(select => {
+        const sectionId = select.closest('.exercicio-n').id;
+        selectedExercises[sectionId] = select.value;
+    });
+    return selectedExercises;
+}
+
+// Função para restaurar as seleções de exercícios
+function restoreExerciseSelections(selectedExercises) {
+    Object.entries(selectedExercises).forEach(([sectionId, selectedExercise]) => {
+        const select = document.querySelector(`#${sectionId} .exercicio__nome`);
+        if (select) {
+            select.value = selectedExercise;
+        }
+    });
+}
